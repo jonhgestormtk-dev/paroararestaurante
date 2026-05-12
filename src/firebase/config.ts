7b42a5f55b741cffd5b68097ca8bec2db7af7c85
Jonh Quadros <jonhgestor.mtk@gmail.com>
2026-05-12 19:02:53 +0000

    apagar as informações da integração do Firebase anterior. Agora vamos c

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,13 +4,13 @@
 /**
  * Configuração do Firebase para o projeto Paroara.
  * IMPORTANTE: Substitua os valores abaixo pelas credenciais geradas no console do Firebase
- * para o projeto 'restaurante-paroara'.
+ * para o NOVO projeto 'restaurante-paroara'.
  */
 export const firebaseConfig = {
-  apiKey: "AIzaSyC-ffccYES59UMeXV6kG4751gIPQsMVr4c", // Substitua pela sua apiKey real
+  apiKey: "AIzaSy_AGUARDE_NOVA_CHAVE", 
   authDomain: "restaurante-paroara.firebaseapp.com",
   projectId: "restaurante-paroara",
   storageBucket: "restaurante-paroara.firebasestorage.app",
-  messagingSenderId: "471751694978",
-  appId: "1:471751694978:web:b20a38faff5e959da3729b"
+  messagingSenderId: "000000000000",
+  appId: "1:000000000000:web:000000000000000000"
 };
