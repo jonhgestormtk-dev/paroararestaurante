825e05e840d246e3267908f5a296202851762d65
Jonh Quadros <jonhgestor.mtk@gmail.com>
2026-05-12 18:19:17 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,14 @@
 
 /**
  * Configuração oficial do Firebase para o projeto Paroara.
- * studio-744554625-e9347
+ * catalog-paroara-db
  */
 export const firebaseConfig = {
-  apiKey: "AIzaSyA2oTnfv2ghHMg-gE6A8myGBHGElaQUQWU",
-  authDomain: "studio-744554625-e9347.firebaseapp.com",
-  projectId: "studio-744554625-e9347",
-  storageBucket: "studio-744554625-e9347.firebasestorage.app",
-  messagingSenderId: "929726693262",
-  appId: "1:929726693262:web:380fad7734bb3393ab6edf"
+  apiKey: "AIzaSyC-ffccYES59UMeXV6kG4751gIPQsMVr4c",
+  authDomain: "catalog-paroara-db.firebaseapp.com",
+  projectId: "catalog-paroara-db",
+  storageBucket: "catalog-paroara-db.firebasestorage.app",
+  messagingSenderId: "471751694978",
+  appId: "1:471751694978:web:b20a38faff5e959da3729b",
+  measurementId: "G-HMZPPLVN3Z"
 };
