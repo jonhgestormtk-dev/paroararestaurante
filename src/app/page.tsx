2c7cb86d1a33ea0c93b1cc289494cc3bb9abbf0e
Jonh Quadros <jonhgestor.mtk@gmail.com>
2026-05-18 11:56:47 +0000

    Trocar a frase como !Seja bem vindo!" (_for element h1_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function SplashPage() {
       
       <div className="max-w-6xl w-full z-10 space-y-12">
         <div className="text-center space-y-4">
-          <h1 className="text-5xl md:text-8xl font-subheadline font-bold italic text-marrom-terra leading-tight">Grupo Gastronômico</h1>
+          <h1 className="text-5xl md:text-8xl font-subheadline font-bold italic text-marrom-terra leading-tight">!Seja bem vindo!</h1>
           <p className="text-cinza-organico font-body text-[10px] md:text-sm max-w-2xl mx-auto opacity-60 uppercase tracking-[0.4em] font-bold">Escolha uma de nossas experiências para começar.</p>
         </div>
 
